57f3a5564e5dd681d42b05b9eef4f7cc271cda83
Bastien Aumeunier <bastien.aumeunier@ynov.com>
2023-12-11 19:06:46 +0100

    start auth feature & user model

---
+++ app/Models/User.ts
@@ -0,0 +1,45 @@
+import { randomUUID } from 'crypto'
+import { DateTime } from 'luxon'
+import Hash from '@ioc:Adonis/Core/Hash'
+import { column, beforeSave, BaseModel, beforeCreate } from '@ioc:Adonis/Lucid/Orm'
+
+export default class User extends BaseModel {
+  @column({ isPrimary: true })
+  public id: string
+
+  @column()
+  public email: string
+
+  @column({ serializeAs: null })
+  public password: string
+
+  @column()
+  public rememberMeToken: string | null
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+
+  @beforeCreate()
+  public static async createUUID(user: User) {
+    user.id = randomUUID()
+  }
+
+  @beforeSave()
+  public static async hashPassword(user: User) {
+    if (user.$dirty.password) {
+      user.password = await Hash.make(user.password)
+    }
+  }
+
+  //@hasOne(() => Profile)
+  //public profile: HasOne<typeof Profile>:
+
+  //@hasOne(() => Subscription)
+  //public subscription: HasOne<typeof Subscription>:
+
+  //@hasOne(() => Company)
+  //public company: HasOne<typeof Company>:
+}
